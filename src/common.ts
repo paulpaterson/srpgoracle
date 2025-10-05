64e7b293965d8f4bc1702267ebbaa58cfee4c533
Paul Paterson <ppaterson@gmail.com>
2025-10-05 14:09:42 -0500

    Random number

--- src/common.ts
+++ src/common.ts
@@ -1,3 +1,7 @@
 export function getGreeting(name: string): string {
-    return `Hello, ${name}! Welcome to the app.`;
+    return `Hello, ${name}! Welcome to the app. v2.1.3`;
+}
+
+export function randomNumber(max: number): number {
+    return Math.floor(1 + Math.random() * max);
 }
--- test/basic_tests.ts
+++ test/basic_tests.ts
@@ -1,10 +1,26 @@
-import {getGreeting} from "../src/common";
+import {randomNumber} from "../src/common";
 
 describe('A simple test', () => {
     test('one', () => {
         expect(10).toEqual(10);
     })
-    test('greeting', () => {
-        expect(getGreeting('bob')).toEqual('Hello, bob! Welcome to the app.')
+})
+
+describe('Random number tests', () => {
+    test('Should be between 1 and max', () => {
+        let seen: Set<number> = new Set<number>();
+
+        // Collect some data
+        for (let idx = 0; idx < 1000; idx++) {
+            let answer = randomNumber(10);
+            seen.add(answer);
+            expect(answer).toBeGreaterThanOrEqual(1);
+            expect(answer).toBeLessThanOrEqual(10);
+        }
+
+        // Check all members seen
+        for (let idx = 1; idx <= 10; idx++) {
+            expect(seen).toContain(idx);
+        }
     })
 })
