5f3d24c792cf2a08649fe2fe0d43c3e3525d260e
Paul Paterson <ppaterson@gmail.com>
2025-10-08 12:37:27 -0500

    Added more types

--- src/ui.ts
+++ src/ui.ts
@@ -94,16 +94,20 @@ ALL_CHOICES.addChoice(new Choice(
         O('Woman', ['woman']),
         O('Older man', ['man']),
         O('Older woman', ['woman']),
+        O('Teenager male', ['young']),
+        O('Teenager female', ['young']),
+        O('Child male', ['young']),
+        O('Child female', ['young']),
 ]));
 
 ALL_CHOICES.addChoice(new Choice(
     "Disposition", [
-        O('Friendly'),
-        O('Open'),
-        O('Neutral'),
-        O('Wary'),
-        O('Suspicious'),
-        O('Angry'),
+        O('Friendly', ['Mostly positive']),
+        O('Open', ['Mostly positive']),
+        O('Neutral', ['Mostly positive', 'Mostly negative']),
+        O('Wary', ['Mostly positive', 'Mostly negative']),
+        O('Suspicious', ['Mostly negative']),
+        O('Angry', ['Mostly negative']),
 ]));
 
 ALL_CHOICES.addChoice(new Choice(
@@ -147,6 +151,37 @@ ALL_CHOICES.addChoice(new Choice(
         O("An emotion", ["concept"]),
         O("An event", ["concept"]),
         O("A disaster", ["concept"]),
+]));
+
+ALL_CHOICES.addChoice(new Choice(
+    "Humor", [
+        O("Physical", []),
+        O("Slapstick", []),
+        O("Edge/Offensive", []),
+        O("Gallows or Dark", []),
+        O("Deadpan or Dry", []),
+        O("Satirical/Parody/Spoof", []),
+        O("Wordplay", []),
+        O("Witty or Highbrow", []),
+        O("Ironic", []),
+        O("Bodily/Earthy/Lowbrow", []),
+]));
+
+ALL_CHOICES.addChoice(new Choice(
+    "Quests", [
+        O("Heist or infiltration", []),
+        O("Break a siege", []),
+        O("Scout or spy mission", ['non-fantasy']),
+        O("Monster hunt or drive dangers out", []),
+        O("Investigate danger, crime", ['non-fantasy']),
+        O("Discover or find a lost item", ['non-fantasy']),
+        O("Fetch quest", ['non-fantasy']),
+        O("Escort or delivery mission", ['non-fantasy']),
+        O("Obtain a treasure that is guarded", ['non-fantasy']),
+        O("Lift a curse or deal with cursed item", []),
+        O("Disaster, plague, strange weather etc", []),
+        O("Deal with injustice or fight polical corruption", ['non-fantasy']),
+        O("Enchanted journey", []),
 ]));
 
 
